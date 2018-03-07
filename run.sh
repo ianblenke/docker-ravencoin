e86effbf48bec621703df86bd7ebba0ef5be112f
Ian Blenke <ian@blenke.com>
2018-03-06 21:53:15 -0500

    Allow parameter to be passed

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NUM_FALLBACK_THREADS=${NUM_FALLBACK_THREADS:-$(( $(nproc) * 2 ))}
 
-exec ccminer -a x16r --num-fallback-threads $NUM_FALLBACK_THREADS -o stratum+tcp://cryptopool.party:3636 -u RRgfXWgAXhzrYdA1Jn91iFNhjyhV4C7TJB
+exec ccminer -a x16r --num-fallback-threads $NUM_FALLBACK_THREADS -o stratum+tcp://cryptopool.party:3636 -u RRgfXWgAXhzrYdA1Jn91iFNhjyhV4C7TJB $@
